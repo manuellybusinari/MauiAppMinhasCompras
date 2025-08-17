b1fdd5e4653a9bf9d8fccecf8f55503f84ce3e07
manuellybusinari <manuellybusinari@outlook.com>
2025-08-17 19:50:38 -0300

    Adicionar arquivos de projeto.

---
+++ Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,34 @@
+﻿using SQLite;
+using MauiAppMinhasCompras.Models;
+
+namespace MauiAppMinhasCompras.Helpers
+{
+    public class SQLiteDatabaseHelper
+    {
+        private SQLiteAsyncConnection _database;
+
+        public SQLiteDatabaseHelper(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> SalvarProdutoAsync(Produto produto)
+        {
+            if (produto.Id != 0)
+                return _database.UpdateAsync(produto);
+            else
+                return _database.InsertAsync(produto);
+        }
+
+        public Task<List<Produto>> GetProdutosAsync()
+        {
+            return _database.Table<Produto>().ToListAsync();
+        }
+
+        public Task<int> DeleteProdutoAsync(Produto produto)
+        {
+            return _database.DeleteAsync(produto);
+        }
+    }
+}
