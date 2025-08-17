0b4b893caa9a4e64222cdfd6ab43b22e2436262d
manuellybusinari <manuellybusinari@outlook.com>
2025-08-17 20:42:01 -0300

    Construção da classe: CRUD no SQLite

--- Helpers/SQLiteDatabaseHelper.cs
+++ Helpers/SQLiteDatabaseHelper.cs
@@ -5,30 +5,50 @@ namespace MauiAppMinhasCompras.Helpers
 {
     public class SQLiteDatabaseHelper
     {
-        private SQLiteAsyncConnection _database;
+        readonly SQLiteAsyncConnection _conn;
 
-        public SQLiteDatabaseHelper(string dbPath)
+        private SQLiteDatabaseHelper(string path)
         {
-            _database = new SQLiteAsyncConnection(dbPath);
-            _database.CreateTableAsync<Produto>().Wait();
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+        
+        public Task<int> Insert(Produto p) 
+        {
+            return _conn.InsertAsync(p);
+        }
+        //ok
+
+        public Task<List<Produto>> Update (Produto p) 
+        {
+            string sql = "UPTADE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
+
+            return _conn.QueryAsync<Produto>(
+                sql, p.Descricao, p.Quantidade, p.Preco, p.Id
+                
+             );
+        }
+        //ok
+
+        public Task<int> Delete(int id) 
+        { 
+           return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        
+        }
+        //ok
+
+        public Task<List<Produto>> GetAll() 
+        {
+           return _conn.Table<Produto>().ToListAsync();     
+        }
+        //ok
+        public Task<List<Produto>> Search(string q) 
+        {
+            string sql = "SELECT * Produto WHERE descricao LIKE '%" + q + "%'";
+
+            return _conn.QueryAsync<Produto>(sql);
         }
 
-        public Task<int> SalvarProdutoAsync(Produto produto)
-        {
-            if (produto.Id != 0)
-                return _database.UpdateAsync(produto);
-            else
-                return _database.InsertAsync(produto);
-        }
-
-        public Task<List<Produto>> GetProdutosAsync()
-        {
-            return _database.Table<Produto>().ToListAsync();
-        }
-
-        public Task<int> DeleteProdutoAsync(Produto produto)
-        {
-            return _database.DeleteAsync(produto);
+      
         }
     }
-}
