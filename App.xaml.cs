d7471af07d8e1c661385aefea0707ec0696a9e42
manuellybusinari <manuellybusinari@outlook.com>
2025-08-25 00:12:44 -0300

    AGENDA3

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,40 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+
+        static SQLiteDatabaseHelper _db; //_db -> campo
+
+        public static SQLiteDatabaseHelper Db // -->Db: propriedade de leitura
+        { get // contendo uma instância da classe SQLiteDatabaseHelper
+            {
+                if (_db == null) // se não houver nenhum objeto dentro do campo
+                {
+                    //fazer o mecanismo de multiplataforma funcionar corretamente: 
+                    //caminho até o arquivo db3 (dado em vídeoaula)
+
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData), //Local que contém os arquivos da aplicação
+                              "banco_sqlite_compras.db3"); // nome do arquivo
+
+                    // Combine: Encapsulamento com caminhos diretos e completos
+                    // Environment: informações sobre o ambiente
+                    // GetFolderPath: Informações contidas em uma pasta
+                    // Environment.SpecialFolder: Local/caminho
+
+                   
+
+
+                    _db= new SQLiteDatabaseHelper(path);
+                }
+                return _db; // retorna
+            }            
+            
+                }
+                                             
         public App()
         {
             InitializeComponent();
--- Views/NovoProduto.xaml.cs
+++ Views/NovoProduto.xaml.cs
@@ -1,3 +1,6 @@
+using MauiAppMinhasCompras.Models;
+using System.Threading.Tasks;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +9,30 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			// Preecher a Model -> produto
+			Produto p = new Produto // declaração da variável 'p' do tipo produto
+			{
+				Descricao= txt_descricao.Text,
+				Quantidade= Convert.ToDouble(txt_qtd.Text),
+				Preco= Convert.ToDouble(txt_preco.Text)
+			};  // MODEL preenchida
+
+			// 'space' no teclado lista todas as propriedades para fazer o preenchimento do objeto 
+
+			await App.Db.Insert(p);
+			//Avisar o usuário que deu tudo certo com DisplayAlert
+			await DisplayAlert("Sucesso", "Registro inserido", "OK!!");
+
+		}catch(Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "OK!"); 
+		}
+	
+
+    }
 }
